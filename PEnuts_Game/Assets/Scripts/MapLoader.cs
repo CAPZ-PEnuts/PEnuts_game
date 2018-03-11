021810a2c747b11559b203c315ef1cff5237a0b4
Celivalg <>
2018-03-11 12:58:09 +0100

    MapLoader begun
    Started implementing a map loader for each player, not ready to compile at all

---
+++ PEnuts_Game/Assets/Scripts/MapLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MapLoader : MonoBehaviour {
+
+    private int PlayerID;
+    private 
+
+    MapLoader(string file, int playerID)
+    {
+        bool fileLoaded = false;
+        try
+        {
+            FileStream map = File.Open(file, FileMode.Open);
+            fileLoaded = true;
+        }
+        catch
+        {
+
+        }
+
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    
+
+	
+}
