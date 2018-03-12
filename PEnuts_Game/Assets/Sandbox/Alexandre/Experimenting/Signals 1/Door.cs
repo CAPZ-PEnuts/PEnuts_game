2f42b5ccf7673fb55585047fb059c276aede6e8f
Celivalg <>
2018-03-12 15:50:16 +0100

    signals v0.1

---
+++ PEnuts_Game/Assets/Sandbox/Alexandre/Experimenting/Signals 1/Door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+	public string signal;
+	public GameObject signalHandelerObject;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (signal != null && signalHandelerObject != null)
+		{
+			SignalHandeler signalHandeler = signalHandelerObject.GetComponent<SignalHandeler>();
+			if (signalHandeler != null && signalHandeler.Exists(signal) && gameObject.activeSelf != signalHandeler.GetSignal(signal))
+				gameObject.SetActive(!gameObject.activeSelf);
+		}
+	}
+}
---
+++ PEnuts_Game/Assets/Sandbox/Alexandre/Experimenting/Signals 1/Pressure Plate.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+
+	public string signal;
+	public GameObject signalHandelerObject;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (signal != null)
+		{
+			SignalHandeler signalHandeler = signalHandelerObject.GetComponent<SignalHandeler>();
+			if (signalHandeler != null)
+			{
+				if (!signalHandeler.Exists(signal))
+					signalHandeler.AddSignal(signal,true);
+				else
+					signalHandeler.SetSignal(signal,true);
+			}
+		}
+	}
+	
+	private void OnTriggerExit(Collider other)
+	{
+		if (signal != null)
+		{
+			SignalHandeler signalHandeler = signalHandelerObject.GetComponent<SignalHandeler>();
+			if (signalHandeler != null)
+			{
+				if (!signalHandeler.Exists(signal))
+					signalHandeler.AddSignal(signal,true);
+				else
+					signalHandeler.SetSignal(signal,true);
+			}
+		}
+	}
+}
