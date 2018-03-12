a7c84ffcd1ab026e9806b5dce7d51d72eae4f702
Fayln <>
2018-03-12 15:34:53 +0100

    Aucun changement juste je push

---
+++ PEnuts_Game/Assets/Sandbox/Cloé/code/scenechanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class scenechanger : MonoBehaviour {
+
+    public string scenetogo;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void changescene()
+    {
+        //SceneManager.UnloadScene("DontDestroyOnLoad");
+        SceneManager.LoadScene(scenetogo);
+    }
+        
+}
