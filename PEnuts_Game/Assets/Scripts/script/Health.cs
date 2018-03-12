1e88aa50e3d27ef53907d14e5700839501da5d9a
zianely <35142682+zianely@users.noreply.github.com>
2018-03-12 17:21:16 +0100

    debut kill zone

--- PEnuts_Game/Assets/Scripts/script/Health.cs
+++ PEnuts_Game/Assets/Scripts/script/Health.cs
@@ -12,7 +12,8 @@ public class Health : MonoBehaviour
         currentHealth -= amout;
         if(currentHealth <= 0)
         {
-            currentHealth = 0;
+            currentHealth = maxhealth;
+            transform.localPosition = Vector3.zero;
             Debug.Log("dead!");
         }
         healthbar.sizeDelta = new Vector2(currentHealth, healthbar.sizeDelta.y); 
