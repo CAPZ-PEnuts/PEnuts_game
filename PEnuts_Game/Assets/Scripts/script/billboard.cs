af10dcb9d0ee2df70c7ac68dd4086f788b9c5294
zianely <35142682+zianely@users.noreply.github.com>
2018-03-12 19:17:48 +0100

    CUBEPOUSSERMULTI

---
+++ PEnuts_Game/Assets/Scripts/script/billboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class billboard : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.LookAt(Camera.main.transform);
+	}
+}
