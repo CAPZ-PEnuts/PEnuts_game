535476e1874dca0867e9d1405a5a1f7fbfe02de0
Fayln <cloclo.l@orange.fr>
2018-04-28 16:21:24 +0200

    correction du bug sur le cube qu'on pousse

--- PEnuts_Game/Assets/Sandbox/Cloé/code/cubemouv.cs
+++ PEnuts_Game/Assets/Sandbox/Cloé/code/cubemouv.cs
@@ -6,23 +6,25 @@ public class cubemouv : MonoBehaviour {
 
 	public GameObject player;
     private Vector3 mouv = new Vector3(0, 0, 0);
-
+    
     void OnTriggerEnter (Collider other)
 	{
 		if (other.gameObject == player)
 		{
-			other.gameObject.GetComponent<caissePlayer>().Caisse = this; 
+			other.gameObject.GetComponent<caissePlayer>().Caisse = this;            
             mouv.y += 0.01f;
-            transform.position += mouv;             
-		}
+            transform.position += mouv;
+            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation; 
+
+        }
 	}
 
-	void OnTriggerExit (Collider other)
+    void OnTriggerExit (Collider other)
 	{
 		if (other.gameObject == player)
 		{
 			other.gameObject.GetComponent<caissePlayer>().Caisse = null;
-            transform.position -= mouv;
+            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation; 
         }
 	}
 }
