fc38184f5f98b451478b5d85676aadc0835ee77a
Celivalg <>
2018-04-30 15:36:54 +0200

    working on map

---
+++ PEnuts_Game/Assets/Sandbox/Alexandre/Experimenting/Map/GOInstancer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using UnityEngine;
+
+public class GOInstancer : MonoBehaviour
+{
+
+	public string name;
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+/*
+	private Transform getPrefab()
+	{
+		
+		
+		
+		Resources.Load(Application.dataPath + "/Prefabs/");
+		return 
+	}*/
+}
---
+++ PEnuts_Game/Assets/Sandbox/Alexandre/Experimenting/Map/Map.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+
+	public string name;
+	public int sizex;
+	public int sizey;
+
+	
+	
+	private string data;
+	
+	void Start ()
+	{
+		
+
+	}
+	
+	
+}
