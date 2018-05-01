e987da8e832a60ff82117f550485a72688950cad
Fayln <>
2018-05-01 19:44:39 +0200

    level 5

--- PEnuts_Game/Assets/Sandbox/Cloé/code/boutton.cs
+++ PEnuts_Game/Assets/Sandbox/Cloé/code/boutton.cs
@@ -3,33 +3,17 @@ using System.Threading;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class boutton : MonoBehaviour {
+public class boutton : MonoBehaviour
+{
     public GameObject player;
-    public signalhandlorder signal; 
+    public signalhandlorder signal;
 
-<<<<<<< HEAD
-    void OnTriggerEnter(Collider other)
+
+    void ontriggerenter(Collider other)
     {
-     if (other.gameObject == player)
-        //if (Input.GetKeyDown(KeyCode.F))
-           signal.buttonpush(this);
-=======
-	// Use this for initialization
-	void Start()
-    {
-        porte.mybut[place] = this;
-        Debug.Log(porte.mybut);
-	}
-	
-	void OnTriggerEnter(Collider other)
-    {
-        //if (other.gameObject == player)
-        {
+        if (other.gameObject == player)
             //if (Input.GetKeyDown(KeyCode.F))
-            Debug.Log("Prout53");
-                porte.activebutton(this); 
-        }
->>>>>>> Cloe
-    }
+            signal.buttonpush(this);
 
+    }
 }
