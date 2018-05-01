4c5894e040239cec080ee9ef5797f480e67de1c0
Fayln <>
2018-05-01 14:43:19 +0200

    Do not mind this

--- PEnuts_Game/Assets/Sandbox/Cloé/code/boutton.cs
+++ PEnuts_Game/Assets/Sandbox/Cloé/code/boutton.cs
@@ -7,11 +7,29 @@ public class boutton : MonoBehaviour {
     public GameObject player;
     public signalhandlorder signal; 
 
+<<<<<<< HEAD
     void OnTriggerEnter(Collider other)
     {
      if (other.gameObject == player)
         //if (Input.GetKeyDown(KeyCode.F))
            signal.buttonpush(this);
+=======
+	// Use this for initialization
+	void Start()
+    {
+        porte.mybut[place] = this;
+        Debug.Log(porte.mybut);
+	}
+	
+	void OnTriggerEnter(Collider other)
+    {
+        //if (other.gameObject == player)
+        {
+            //if (Input.GetKeyDown(KeyCode.F))
+            Debug.Log("Prout53");
+                porte.activebutton(this); 
+        }
+>>>>>>> Cloe
     }
 
 }
---
+++ PEnuts_Game/Assets/Sandbox/Cloé/code/porte.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Threading;
+using System; 
+using System.Collections.Generic;
+using UnityEngine;
+
+public class porte : MonoBehaviour {
+
+    private Stack<boutton> buttons;
+    public boutton[] mybut;
+    public int nbelmt;
+
+    public porte()
+    {
+        
+    }
+
+    void Start()
+    {
+        mybut = new boutton[nbelmt];
+        buttons = new Stack<boutton>();
+        Thread.Sleep(3);
+        foreach (boutton but in mybut)
+            buttons.Push(but);
+    }
+    
+
+    
+   /*void Start()
+    {
+        
+    }*/
+
+    public void activebutton (boutton bouton)
+    {
+        Debug.Log("Prout");
+        if (bouton == buttons.Peek())
+        {
+            buttons.Pop();
+            destroyit(); 
+        }
+    }
+
+    private void destroyit()
+    {
+        if (buttons.Peek().place == nbelmt - 1)
+            Destroy(this.gameObject); 
+    }
+
+}
