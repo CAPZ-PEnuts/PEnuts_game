96741e6c1c573bf7da202669daaaa35bef9d367f
Celivalg <>
2018-05-01 15:54:29 +0200

    a

--- PEnuts_Game/Assets/Sandbox/Cloé/code/boutton.cs
+++ PEnuts_Game/Assets/Sandbox/Cloé/code/boutton.cs
@@ -5,31 +5,13 @@ using UnityEngine;
 
 public class boutton : MonoBehaviour {
     public GameObject player;
-    public signalhandlorder signal; 
+    public signalhandlorder signal;
 
-<<<<<<< HEAD
     void OnTriggerEnter(Collider other)
     {
-     if (other.gameObject == player)
-        //if (Input.GetKeyDown(KeyCode.F))
-           signal.buttonpush(this);
-=======
-	// Use this for initialization
-	void Start()
-    {
-        porte.mybut[place] = this;
-        Debug.Log(porte.mybut);
-	}
-	
-	void OnTriggerEnter(Collider other)
-    {
-        //if (other.gameObject == player)
-        {
+        if (other.gameObject == player)
             //if (Input.GetKeyDown(KeyCode.F))
-            Debug.Log("Prout53");
-                porte.activebutton(this); 
-        }
->>>>>>> Cloe
+            signal.buttonpush(this);
     }
 
 }
