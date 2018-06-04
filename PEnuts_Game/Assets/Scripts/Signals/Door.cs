4163ea1edede9f8f21443d443797ff180c350bce
Pierrick-MADE <pierrick.made@epita.fr>
2018-06-04 17:14:53 +0200

    Implemented Door but some bugs have to be fixed

--- PEnuts_Game/Assets/Scripts/Signals/Door.cs
+++ PEnuts_Game/Assets/Scripts/Signals/Door.cs
@@ -8,10 +8,13 @@ public class Door : MonoBehaviour
 	public string signal;
 	public GameObject signalHandelerObject;
 	public GameObject doorObject;
+
+	private bool _colorUpdated;
 	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		_colorUpdated = false;
 	}
 	
 	// Update is called once per frame
@@ -21,6 +24,12 @@ public class Door : MonoBehaviour
 			SignalHandeler signalHandeler = signalHandelerObject.GetComponent<SignalHandeler>();
 			if (signalHandeler != null /*&& signalHandeler.Exists(signal)*/ && doorObject.activeSelf != !signalHandeler.GetSignal(signal))
 				doorObject.SetActive(!doorObject.activeSelf);
+			if (!_colorUpdated)
+			{
+				doorObject.GetComponent<Material>().SetColor("_Color",signalHandelerObject.GetComponent<SignalHandeler>().GetSignalColor(signal));
+			}
 		}
+
+		
 	}
 }
