48796ac4eb57c40f9c1df93f677d592a3bb82c7b
Pierrick-MADE <pierrick.made@epita.fr>
2018-06-04 14:45:24 +0200

    Script changement de camera <3

--- PEnuts_Game/Assets/Sandbox/Cloé/code/tpvector0.cs
+++ PEnuts_Game/Assets/Sandbox/Cloé/code/tpvector0.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class tpvector0 : MonoBehaviour {
 
-	public GameObject player;
 	public Vector3 where2go;
 	
 	void OnTriggerEnter(Collider other)
 	{
-		if (other.gameObject == player)
-			player.transform.position = where2go;
+		if (other.gameObject.tag == "player")
+			other.transform.position = where2go;
 	}
 }
