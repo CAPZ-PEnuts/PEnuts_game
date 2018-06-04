7387360bff1237f1070a48e115fa9deafd0f1e8d
Pierrick-MADE <pierrick.made@epita.fr>
2018-06-04 16:06:37 +0200

    fini les cameras

--- PEnuts_Game/Assets/Sandbox/Cloé/code/changercamera.cs
+++ PEnuts_Game/Assets/Sandbox/Cloé/code/changercamera.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class changercamera : MonoBehaviour {
+public class changercamera : NetworkBehaviour {
 
-    public Camera Main;
-    public Camera cameraToGo; 
+    Camera Main;
+    Camera cameraToGo;
 
+    public void Start()
+    {
+        Main = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        cameraToGo = GameObject.FindWithTag("camera2").GetComponent<Camera>();
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "player")
+        if (isLocalPlayer && other.gameObject.tag == "tp")
         {
             Main.enabled = false;
             cameraToGo.enabled = true; 
