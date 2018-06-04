6dd369a0442fc0f87c196f4e699c08b949254c12
Pierrick-MADE <pierrick.made@epita.fr>
2018-06-04 14:57:34 +0200

    added sfx mp3 / camera

---
+++ PEnuts_Game/Assets/Sandbox/Cloé/code/changercamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changercamera : MonoBehaviour {
+
+    public Camera Main;
+    public Camera cameraToGo; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            Main.enabled = false;
+            cameraToGo.enabled = true; 
+        }
+    }
+
+}
---
+++ PEnuts_Game/Assets/Sandbox/Cloé/code/tpvector0.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tpvector0 : MonoBehaviour {
+
+	public Vector3 where2go;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "player")
+			other.transform.position = where2go;
+	}
+}
