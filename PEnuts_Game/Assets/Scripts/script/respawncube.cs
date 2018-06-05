a88e5aed0da4fc2eeb68abde232e78be805a03fe
Fayln <cloe.lacombe@epita.com>
2018-06-05 15:07:07 +0200

    some cude fixes, need to be tested

--- PEnuts_Game/Assets/Scripts/script/respawncube.cs
+++ PEnuts_Game/Assets/Scripts/script/respawncube.cs
@@ -7,6 +7,16 @@ public class respawncube : MonoBehaviour
 
 	public GameObject caissespawn;
 
+	private void Update()
+	{
+		Vector3 v = GetComponent<Rigidbody>().velocity;
+		if (v.magnitude <= 0.1 &&  (transform.rotation.eulerAngles.y > 5 || transform.rotation.eulerAngles.y < -5 || transform.rotation.eulerAngles.z > 5 || transform.rotation.eulerAngles.z < -5)  )
+		{
+			transform.eulerAngles = caissespawn.transform.eulerAngles;
+			transform.position = caissespawn.transform.position; 
+		}
+	}
+
 	private void OnCollisionEnter(Collision other)
 	{
 		if (other.gameObject.tag == "Killzone")
