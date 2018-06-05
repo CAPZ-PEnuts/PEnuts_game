b9eab14cfac4a8fd5d389d775f9a7ca3c11dc566
Fayln <cloe.lacombe@epita.com>
2018-06-05 16:52:25 +0200

    somes fixes on lvl 1 and respawn cube

--- PEnuts_Game/Assets/Scripts/script/respawncube.cs
+++ PEnuts_Game/Assets/Scripts/script/respawncube.cs
@@ -10,7 +10,7 @@ public class respawncube : MonoBehaviour
 	private void Update()
 	{
 		Vector3 v = GetComponent<Rigidbody>().velocity;
-		if (v.magnitude <= 0.1 &&  (transform.rotation.eulerAngles.y > 5 || transform.rotation.eulerAngles.y < -5 || transform.rotation.eulerAngles.z > 5 || transform.rotation.eulerAngles.z < -5)  )
+		if (v.magnitude <= 0.1 &&  (transform.rotation.eulerAngles.x > 10 || transform.rotation.eulerAngles.x < -10 || transform.rotation.eulerAngles.z > 10 || transform.rotation.eulerAngles.z < -10)  )
 		{
 			transform.eulerAngles = caissespawn.transform.eulerAngles;
 			transform.position = caissespawn.transform.position; 
--- PEnuts_Game/Assets/Scripts/script/teleportation.cs
+++ PEnuts_Game/Assets/Scripts/script/teleportation.cs
@@ -8,24 +8,35 @@ public class teleportation : NetworkBehaviour
     public GameObject player;
     public string scenename;
 
+    private Component firstPlayer;
+
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log("Triggered");
         Component pc = other.gameObject.GetComponent("PlayerController");
         if (pc != null)
         {
-            Debug.Log("Teleported");
-            
-            //SceneManager.LoadScene(scenename);
-
-            foreach (GameObject p in GameObject.FindGameObjectsWithTag("NetworkPlayer"))
+            if (firstPlayer == null)
             {
-                NetworkPlayer player = p.GetComponent<NetworkPlayer>();
-                if (player.id == 1)
+                firstPlayer = pc;
+                Debug.Log("First Player checked portal");
+            }
+            else if (firstPlayer.GetInstanceID() != pc.GetInstanceID())
+            {
+                Debug.Log("Teleported");
+            
+                //SceneManager.LoadScene(scenename);
+
+                foreach (GameObject p in GameObject.FindGameObjectsWithTag("NetworkPlayer"))
                 {
-                    player.ChangeScene(scenename);
+                    NetworkPlayer player = p.GetComponent<NetworkPlayer>();
+                    if (player.id == 1)
+                    {
+                        player.ChangeScene(scenename);
+                    }
                 }
             }
+            
         }
     }
 }
